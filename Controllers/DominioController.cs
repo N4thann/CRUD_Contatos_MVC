1bcc97c118fe5e240edd17c50fff7a263a79cbc9
Nathan Farias <nathan90055@gmail.com>
2022-10-14 22:03:46 -0300

    Criado filtro para domínios de emails

--- Controllers/DominioController.cs
+++ Controllers/DominioController.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ContatosMVC.Data;
@@ -12,11 +12,54 @@ namespace ContatosMVC.Controllers
 {
     public class DominioController : Controller
     {
-        // GET: DominioController
-        public ActionResult Gmail()
+       private readonly NZContext _context;
+
+       public DominioController(NZContext context)
+       {
+                _context = context;
+       }
+       public async Task<IActionResult> Index()
+       {
+          return View();
+       }
+
+      public async Task<IActionResult> Gmail()
+       {
+            var contatosGmail = from s in _context.Contatos select s;
+
+            contatosGmail = contatosGmail.Where(s => s.Email.Contains("gmail"));
+
+
+            return View(await contatosGmail.ToListAsync());
+       }
+
+       public async Task<IActionResult> Hotmail()
         {
-            return View();
+            var contatosHotmail = from s in _context.Contatos select s;
+
+            contatosHotmail = contatosHotmail.Where(s => s.Email.Contains("hotmail"));
+
+            return View(await contatosHotmail.ToListAsync());
         }
+        public async Task<IActionResult> Outlook()
+        {
+            var contatosOutlook = from s in _context.Contatos select s;
+
+            contatosOutlook = contatosOutlook.Where(s => s.Email.Contains("outlook"));
+
+            return View(await contatosOutlook.ToListAsync());
+        }
+        public async Task<IActionResult> Outros()
+        {
+            var contatosOutros = from s in _context.Contatos select s;
+
+            contatosOutros = contatosOutros.Where(s => !s.Email.Contains("gmail") &&
+                                                    !s.Email.Contains("outlook") &&
+                                                    !s.Email.Contains("hotmail"));
+            return View(await contatosOutros.ToListAsync());
+        }
+
+
 
     }
 }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace ContatosMVC.Controllers
             return View();
         }
 
+        public IActionResult Dominio()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
